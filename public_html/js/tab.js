3efec2d4b937bbbec5062b12dd278cc5088ddd59
Loki88 <laplacetransformed@gmail.com>
2014-02-23 00:52:19 +0100

    Prototipo completato

--- public_html/js/tab.js
+++ public_html/js/tab.js
@@ -3,25 +3,61 @@ var Tab = {
     
     current: null,
     
+    time: 500,
+    
     init: function()
     {
+        //Stato iniziale
+//       var state = {
+//           href: location.href,
+//       };
+//       history.pushState(state, 'state', location.href);
        //Recuperare il menù della tab e collegare ogni bottone al relativo spazio in cui visualizzare il contenuto
        var tab_elements = $('.tab-element');
-       console.log('tab_elements', tab_elements.length);
        for(var i=0; i<tab_elements.length; i++)
        {
            var element = $(tab_elements[i]);
            console.log('tab-element', element);
            Tab.prepareElement(element, i);
        }
+       $(window).on('fluidGrid', function(event){
+            var link = $(Tab.current);
+            if(!link.prop(open))
+            {
+                var page = $(link.prop('page'));
+                var element = link.parent('.tab-element');
+                element.addClass('section group current');
+                page.show();
+            }   
+        });
+        $(window).on('noMobile', function(event){
+            var link = $(Tab.current);
+            if(!link.prop(open))
+            {
+                var page = $(link.prop('page'));
+                var element = link.parent('.tab-element');
+                element.addClass('section group current');
+                page.show();
+            }  
+        });
+        
+        $(window).on('popstate', function(event){
+            event.preventDefault();
+            var state = event.originalEvent.state;
+            if(state != null)
+                window.location.assign(state.href);
+            else
+                window.location.assign(window.location.href);
+        });
     },
     
-    prepareElement: function(element)
+    prepareElement: function(element, i)
     {
         element = $(element);
         
         var link = $(element.find('.tab-link')[0]);
-        
+        var id = 'tab_link_'+i;
+        link.attr('id', id);
         if(element.hasClass('current'))
         {
             Tab.current = link;
@@ -45,6 +81,7 @@ var Tab = {
         event.preventDefault();
         $(document.body).css('cursor', 'wait');
         var link = $(this);
+        
         if(!link.prop('setted'))
         {
             var dataUrl = $(this).attr('href');
@@ -61,6 +98,7 @@ var Tab = {
                         content.hide();
                         link.prop('page', link.next('.tab-page'));
                         link.prop('setted', true);
+                        
                     }
                 },
                 complete: function()
@@ -71,6 +109,7 @@ var Tab = {
         }
         else
             Tab.changePage(link);
+        Tab.pushHistory(link);
     },
     
     changePage: function(link)
@@ -81,13 +120,15 @@ var Tab = {
         if(width > 481)
         {
             Tab.current = link;
-            $(currentLink.prop('page')).fadeOut(800, function(){
+            
+            $(currentLink.prop('page')).fadeOut(Tab.time, function(){
                 currentLink.parent('.tab-element').removeClass('section group current');
+                currentLink.prop('open', false);
                 var element = link.parent('.tab-element');
                 element.addClass('section group current');
                 var page = $(link.prop('page'));
-                console.log('page', page);
-                page.fadeIn(800, function(){
+                page.fadeIn(Tab.time, function(){
+                    page.prop('open', true);
                     $(document.body).css('cursor', 'auto');
                 });
             });
@@ -96,6 +137,7 @@ var Tab = {
         {
             Tab.accordion(link);
         }
+        
     },
     
     accordion: function(link)
@@ -105,26 +147,29 @@ var Tab = {
         if(link.parent('.tab-element').hasClass('current'))
         {
             var page = $(link.prop('page'));
-            page.slideUp(800, function(){
+            page.slideUp(Tab.time, function(){
                 page.parent('.tab-element').removeClass('section group current');
                 $(document.body).css('cursor', 'auto');
+                link.prop('open', false);
             });
-            Tab.current = null;
         }
         else{
             Tab.current = link;
             if(currentLink != undefined)
-            {    $(currentLink.prop('page')).slideUp(800, function(){
+            {    $(currentLink.prop('page')).slideUp(Tab.time, function(){
                    currentLink.parent('.tab-element').removeClass('section group current');
+                   currentLink.prop('open', false);
                });
                link.parent('.tab-element').addClass('section group current');
-                $(link.prop('page')).slideDown(800, function(){
+                $(link.prop('page')).slideDown(Tab.time, function(){
                     $(document.body).css('cursor', 'auto');
+                    link.prop('open', true);
                 });
             }
             else
-               $(link.prop('page')).slideDown(800, function(){
+               $(link.prop('page')).slideDown(Tab.time, function(){
                    link.parent('.tab-element').addClass('section group current');
+                   link.prop('open', true);
                });
             
         }
@@ -134,15 +179,21 @@ var Tab = {
     closeAccordion: function(link)
     {
         link = $(link);
-        $(link.prop('page')).slideUp(800);
+        $(link.prop('page')).slideUp(Tab.time);
+        link.prop('open', false);
         link.parent('.tab-element').removeClass('section group current');
     },
 
     pushHistory: function(link)
     {
-        
+        link = $(link);
+        var id = link.attr('id');
+        var state = {
+            href: link.attr('href'),
+            link_id: id,
+        };
+        history.pushState(state, 'state', link.attr('href'));
     }
-
 };
 
 $(document).ready(Tab.init());
