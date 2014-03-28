0fc9eb370b93a7dbac22cad00825f9ebf81b5df7
Loki88 <laplacetransformed@gmail.com>
2014-03-28 12:54:08 +0100

    Prototipo di comunicazione

--- public_html/js/maps/map.js
+++ public_html/js/maps/map.js
@@ -27,56 +27,23 @@ function initialize() {
 		}
 	];
 
-
 	var mapOptions = {
 		center: fbSpedLatLng,
 		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 
-	var contentString = '<div id="content" class="infoWindow">'+
-		'<div class="header">'+
-                '<h4 class="title">Uffici FB Sped</h4>'+
-                '</div>'+
-		'<p>Via V. Pilotti c\\o Centro Commerciale Gran Sasso, Teramo (TE) 64100</p>'+
-		'</div>';
-	//FARE IN MODO CHE LA INFOWINDOW RISPONDA ALL'HOVER SUL MARKER E SI POSIZIONI SOPRA AL PUNTATORE DEL MOUSE
-
 	var map = new google.maps.Map(document.getElementById("map-canvas"),
 	    mapOptions);
 
 	map.setOptions({styles: styleArray});
-
-//	var infowindow = new google.maps.InfoWindow({
-//	    content: contentString,
-//	    position: fbSpedLatLng,
-//	    maxWidth: '300px',
-//	    disableAutoPan: true,
-//	});
-//	infowindow.open(map);
-
 	var marker = new google.maps.Marker({
 		title: 'FB Sped S.r.l. - Uffici',
 		map: map,
                 position: fbSpedLatLng,
 	});
 	marker.setVisible(true);
-//        marker.open(map);
         map.setCenter(marker.getPosition());
-//	google.maps.event.addListener(infowindow, 'click', function() {
-//	    map.setZoom(26);
-//	    map.setCenter(infowindow.getPosition());
-//	});
-//
-//	google.maps.event.addListener(infowindow, 'closeclick', function() {
-//		marker.setPosition(fbSpedLatLng);
-//		marker.setVisible(true);
-//	});
-
-	google.maps.event.addListener(marker, 'click', function(){
-		this.setVisible(false);
-		infowindow.open(this.getMap());
-	});
 }
 
 function toggleBounce() {
@@ -87,6 +54,5 @@ function toggleBounce() {
 		marker.setAnimation(google.maps.Animation.BOUNCE);
 	}
 }
-
 
 google.maps.event.addDomListener(window, 'load', initialize);
